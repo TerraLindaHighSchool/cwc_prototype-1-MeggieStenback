09b7f56cd5b89e1bd292644cbe31fafc9959d05f
Meggie Stenback <ms42524@srcs.loc>
2022-03-24 15:37:48 -0700

    Tutorial 1.3.1 - 1.4.6

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,9 +11,16 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
-    }
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    void Update() {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //Moves the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime); }
 }
